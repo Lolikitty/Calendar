48a02c7ed0f2df5c2fe0046bd2ac01a4c512e8ec
master <aaa1218bbb@yahoo.com.tw>
2015-04-14 18:38:42 +0800

    First Create

---
+++ Assets/Example/Example.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+
+	void Update () {
+		GetComponent<UILabel>().text 
+			= "選擇的日期是：" + Calendar.CHOOSE_YEAR + " 年 "
+				+ Calendar.CHOOSE_MONTH + "月 "
+				+ Calendar.CHOOSE_DAY + "日";
+	}
+}
